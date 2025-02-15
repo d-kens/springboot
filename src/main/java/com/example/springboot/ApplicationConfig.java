1462d394ae70cdc04f146bb63d14c83d3568e71c
d-kens <omondikens1@gmail.com>
2025-02-15 16:13:29 +0300

    springcore concepts

--- src/main/java/com/example/springboot/ApplicationConfig.java
+++ src/main/java/com/example/springboot/ApplicationConfig.java
@@ -1,14 +1,24 @@
 package com.example.springboot;
 
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean()
-    public Test test() {
-        return new Test("Dickens as a variable");
+    @Profile("dev")
+    public Test test1() {
+        return new Test("The First Bean");
+    }
+
+    @Bean()
+    @Profile("prod")
+    public Test test2() {
+        return new Test("The Second Bean");
     }
 }
--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -3,15 +3,26 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 
-		var applicationContext = SpringApplication.run(SpringbootApplication.class, args);
+		var application = new SpringApplication(SpringbootApplication.class);
+
+		application.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "prod"));
+
+		var applicationContext = application.run(args);
 
 		TestService testService = applicationContext.getBean(TestService.class);
 		System.out.println(testService.tellAStory());
+		System.out.println(testService.getJavaVersion());
+		System.out.println(testService.getOsName());
+		System.out.println(testService.getProperty());
+		System.out.println(testService.getCustomProperty());
+		System.out.println(testService.getCustomProperty2());
 	}
 
 }
